2752b7682688253decb37f6efd75d487a8d00d1a
Killayt <surnamename615@gmail.com>
2022-11-23 12:51:57 +0300

    balls

--- cmd/main.go
+++ cmd/main.go
@@ -3,27 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/gorilla/mux"
+	"text/template"
 )
 
-func productsHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
-	response := fmt.Sprintf("id %s", id)
-	fmt.Fprint(w, response)
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "IndexPage")
+type ViewData struct {
+	Available bool
 }
 
 func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/products/{id:[0-9]+}", productsHandler)
-	router.HandleFunc("/articles/{id:[0-8]+}", productsHandler)
-	router.HandleFunc("/", indexHandler)
-	http.Handle("/", router)
+	data := ViewData{
+		Available: true,
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, _ := template.ParseFiles("web/templates/index.html")
+		tmpl.Execute(w, data)
+	})
 
 	fmt.Println("Server is listening...")
 	http.ListenAndServe(":8080", nil)
